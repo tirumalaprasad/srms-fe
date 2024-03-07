f18401a40caf8320596a25b1f8feda5417a17e0d
Tirumala <ks.tirumala@gmail.com>
2024-03-07 04:01:35 -0500

    fixed overflow

--- src/App.tsx
+++ src/App.tsx
@@ -5,50 +5,51 @@ import Main from "./components/Main";
 
 function App() {
     return (
-            <Grid
-                templateAreas={{
-                    base: `"header" "main"`,
-                    md: `"nav main" "nav main"`,
-                    lg: `"nav main" "nav main"`,
-                }}
-                gridTemplateRows={"50px 1fr"}
-                templateColumns={{
-                    base: `1fr`,
-                    md: `250px 1fr `,
-                    lg: `250px 1fr`,
-                }}
-            >
-                <Show below="md">
-                    <GridItem
-                        area="header"
-                        position="sticky"
-                        top="0"
-                        boxShadow="md"
-                        zIndex="sticky"
-                        bg="gray.100"
-                    >
-                        <HamburgerMenu />
-                    </GridItem>
-                </Show>
-
-                <Show above="md">
-                    <GridItem area="nav">
-                        <NavBar />
-                    </GridItem>
-                </Show>
-
-                <GridItem area="main">
-                    <Box
-                        height="100vh"
-                        paddingLeft={5}
-                        paddingRight={5}
-                        marginTop={5}
-                        marginBottom={5}
-                    >
-                        <Main />
-                    </Box>
+        <Grid
+            templateAreas={{
+                base: `"header" "main"`,
+                md: `"nav main" "nav main"`,
+                lg: `"nav main" "nav main"`,
+            }}
+            gridTemplateRows={"50px 1fr"}
+            templateColumns={{
+                base: `1fr`,
+                md: `250px 1fr `,
+                lg: `250px 1fr`,
+            }}
+        >
+            <Show below="md">
+                <GridItem
+                    area="header"
+                    position="sticky"
+                    top="0"
+                    boxShadow="md"
+                    zIndex="sticky"
+                    bg="gray.100"
+                >
+                    <HamburgerMenu />
                 </GridItem>
-            </Grid>
+            </Show>
+
+            <Show above="md">
+                <GridItem area="nav">
+                    <NavBar />
+                </GridItem>
+            </Show>
+
+            <GridItem area="main">
+                <Box
+                    height="100vh"
+                    paddingLeft={5}
+                    paddingRight={5}
+                    marginTop={5}
+                    marginBottom={5}
+                    overflowY="auto"
+                >
+                    <Main />
+                </Box>
+            </GridItem>
+        </Grid>
     );
 }
 
